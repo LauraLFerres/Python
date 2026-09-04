d51b859f451403f4e6ee2a6289ff40ca62cce9ed
LauraLFerres <laura.lferres@gmail.com>
2026-09-04 11:50:58 -0300

    teste

---
+++ PYTHON_AULA2/teste.py
@@ -0,0 +1,7 @@
+tempo_resposta = int(input("Digite o a velocidade da API:"))
+if tempo_resposta <= 100ms:
+    print("Excelente")
+if tempo_resposta => 100 ms:
+    print("Aceitável")
+if tempo_resposta => 300 ms:
+    print("Lento")
